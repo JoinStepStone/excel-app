3a7213484ee0ce5248d2d2b7f862c0a05464a28e
Nauman Ahmed <ahmedNauman97>
2024-08-16 17:40:45 +0200

    updated url

--- src/API/index.js
+++ src/API/index.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "http://127.0.0.1:5000"
+export const BASE_URL = "https://essay-scoring-backend-30a7c9522d44.herokuapp.com" // https://essay-scoring-backend-30a7c9522d44.herokuapp.com  http://127.0.0.1:5000
