10dbc62ccc319bc864830c2c1c337aa2c1eba83e
Nauman Ahmed <naumanahmed449@gmail.com>
2024-11-20 21:42:35 +0100

    Updated BASE URL

--- src/API/index.js
+++ src/API/index.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://essay-scoring-backend-30a7c9522d44.herokuapp.com" 
+export const BASE_URL = "https://stepstone-0cb712457d2b.herokuapp.com/" 
 // export const BASE_URL = "http://127.0.0.1:5000"
